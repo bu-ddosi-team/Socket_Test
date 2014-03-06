f244b743ee5b9113b2f521f41469cfc50bb9cb63
andy2m11 <andy2m11@gmail.com>
2014-03-06 05:02:02 -0500

    start modularizing

---
+++ LT Server Unix/LT Server Console U2/objserver.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include <time.h>
+#include <ctime>
+
+
+struct sockserv
+{
+	/* data */
+};
+
